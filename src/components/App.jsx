c08abeaf992c811c0f56b590d43584f994b3a436
erenduzova <90982361+erenduzova@users.noreply.github.com>
2022-06-25 22:16:27 +0300

    Challange 1 completed and StrictMode used When new text is written into the input, its state should be saved.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [newItem, setNewItem] = useState();
+
+  function handleNewItemChange(event) {
+    const value = event.target.value;
+    setNewItem(value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="New Item"
+          value={newItem}
+          onChange={handleNewItemChange}
+        />
         <button>
           <span>Add</span>
         </button>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 //CHALLENGE: Make this app work by applying what you've learnt.
 //1. When new text is written into the input, its state should be saved.
